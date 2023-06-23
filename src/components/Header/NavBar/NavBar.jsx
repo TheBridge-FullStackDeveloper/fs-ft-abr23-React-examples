61a72d117c754343696212758e755355f4ee150f
Guille-Rubio <guillermorubiog@gmail.com>
2023-06-23 11:11:38 +0200

    ADD/ Routing demo

--- src/components/Header/NavBar/NavBar.jsx
+++ src/components/Header/NavBar/NavBar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './NavBar.css';
 
 const NavBar = () => {
   return <nav >
     <ul className="nav-bar">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/staff">Staff</Link>
-      <Link to="/destinations">Destinations</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to="/rating">Rating</Link>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/staff">Staff</Link></li>
+      <li><Link to="/destinations">Destinations</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
+      <li><Link to="/rating">Rating</Link></li>
     </ul>
   </nav>;
 };
